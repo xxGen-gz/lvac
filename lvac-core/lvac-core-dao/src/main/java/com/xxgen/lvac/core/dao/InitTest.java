557da4b0dfefa532941ad6fe0b731a36cb00bd69
xxGen-gz <xxGen_gz@163.com>
2017-11-29 23:49:52 +0800

    重新组建的第一次提交

---
+++ lvac-core/lvac-core-dao/src/main/java/com/xxgen/lvac/core/dao/InitTest.java
@@ -0,0 +1,36 @@
+
+/** 
+ * @Title 初始化测试 
+ * @Package com.xxgen.lvac.core.dao 
+ * @Desription 初始化测试 
+ * @ClassName InitTest.java
+ * @author xxGen
+ * @CreateDate 2017年11月29日 下午11:13:40
+ * @UpdateUser xxGen
+ * @version v0.1 
+ */
+
+package com.xxgen.lvac.core.dao;
+
+
+/** 
+ * @ClassName InitTest 
+ * @Description 初始化测试 
+ * @author xxGen
+ * @date 2017年11月29日 下午11:13:40 
+ */
+
+public class InitTest {
+
+	/**  
+	 * @Description  初始化测试 
+	 * @author xxGen
+	 * @date 2017年11月29日 下午11:13:40 
+	 *
+	 * @param args 
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
